b16fa1a718f2f5ec9efc66325e61e7a969979818
Elizabeth <mwendemuthusi6@gmail.com>
2022-11-20 04:18:40 +0300

    Assignment to unblock

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+// To fix this
+
+// function writeCards(arr, event) {
+//     for (let i = 0; i < arr.length; i++){
+
+//         const thankyouCard = [];
+//         thankyouCard.slice(0, 0, `Thank you, ${arr[i]}, for the ${event}`)]
+//         console.log(thankyouCard);
+//     }
+//     return arr;
+// }
+
+
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "wonderful surprise gift"));
+
+function countDown() {
+    let i = 10;
+    while (i <= 0) {
+        console.log(i);
+        i--;
+    }
+
+}
+console.log(countDown());
